26bbfedd94ad66427fd4eceb7f1b888c00b1b6dd
Dolmatowknu <dolmatowit@knu.ua>
2022-11-27 19:36:50 +0200

    1

---
+++ app.py
@@ -0,0 +1,39 @@
+import heapq
+from flask import Flask
+
+app = Flask(__name__)
+
+print("pr3")
+
+@app.route('/')
+
+def print_item(box, itemname): #t
+    print(f'There is a price of {itemname}: ')
+    return(box[itemname])
+
+    
+def add_bread(box):
+    itemname = 'bread'
+    itemvalue = 10
+    box[itemname] = {itemvalue}
+    print('There is a basket with new item: ')
+    return(box)
+
+def print_basket_len(box):#t
+    print("Number of items in teh basket: ")
+    return(len(box))
+
+def print_basket(box):
+    print("There is the basket now: ")
+    return(box)
+
+
+box = {'potato': 20, 'meat':70, 'cheese':35, 'tea':15}
+
+print(print_basket(box))
+print(print_basket_len(box))
+print(add_bread(box))
+print(print_item(box, 'meat'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+import app as tapp
+
+class Testing(unittest.TestCase):
+    def test_print_item(self):
+        self.assertEqual(tapp.print_item(tapp.box, 'cheese'), 35)
+
+    def test_print_basket_len(self):
+        self.assertEqual(tapp.print_basket_len(tapp.box), 5)
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
