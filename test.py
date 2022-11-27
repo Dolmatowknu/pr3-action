d950a9ded5f85882d450493f2245b1bd92cf8c7d
Dolmatowknu <dolmatowit@knu.ua>
2022-11-27 20:58:08 +0200

    3

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import app as tapp
 import json
 
 class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = tapp.app.test_client()
+
     def test_print_item(self):
         self.assertEqual(tapp.print_item(tapp.box, 'cheese'), 35)
 
     def test_print_basket_len(self):
         self.assertEqual(tapp.print_basket_len(tapp.box), 5)
-
-    
-
 if __name__ == '__main__':
     unittest.main()
