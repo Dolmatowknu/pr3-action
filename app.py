1dfffc3a56645a60c37e382f59c3573a9fd8110d
Dolmatowknu <dolmatowit@knu.ua>
2022-11-27 21:28:47 +0200

    4

--- app.py
+++ app.py
@@ -14,24 +14,21 @@ def add_bread(box):
     itemname = 'bread'
     itemvalue = 10
     box[itemname] = {itemvalue}
-    print('There is a basket with new item: ')
     return(box)
 
 def print_basket_len(box):
-    print("Number of items in teh basket: ")
     return(len(box))
 
 def print_basket(box):
-    print("There is the basket now: ")
-    return(box)
+    return(f"{box}")
 
 
 box = {'potato': 20, 'meat':70, 'cheese':35, 'tea':15}
 
-print(print_basket(box))
-print(print_basket_len(box))
-print(add_bread(box))
-print(print_item(box, 'meat'))
+print_basket(box)
+#print(print_basket_len(box))
+#print(add_bread(box))
+#print(print_item(box, 'meat'))
 
 if __name__ == '__main__':
     app.run(debug=True)
