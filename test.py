ccb2caa3e6b89add1c5ba6038bddbb039cb0f7fc
Dolmatowknu <dolmatowit@knu.ua>
2022-11-27 21:56:08 +0200

    14

--- test.py
+++ test.py
@@ -10,12 +10,12 @@ class FlaskAppTests(unittest.TestCase):
     def test_print_item(self):
 #        r = self.app.get('/')
 #        self.assertEqual(r._status_code, 200)
-        self.assertEqual(tapp.print_item(tapp.box, 'cheese'), 35)
+        self.assertEqual(tapp.print_item(tapp.box, 'cheese'), '35')
 
     def test_print_basket_len(self):
 #        r = self.app.get('/')
 #        self.assertEqual(r._status_code, 200)
-        self.assertEqual(tapp.print_basket_len(tapp.box), 5)
+        self.assertEqual(tapp.print_basket_len(tapp.box), '5')
 
 if __name__ == '__main__':
     unittest.main()
